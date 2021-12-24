06d4049165cbd4df0ac037e84dc427178f29e535
Salahuddin Ahammed <salahuddinahammed@yahoo.com>
2021-12-24 11:53:05 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Frontend Mentor Solutions
+
+These are my submissions of [Frontend Mentor](https://frontendmentor.io)
+challenges.
+
+See projects in [live](https://frontend-mentor-solutions-salahuddin.netlify.app).
---
+++ app.js
@@ -0,0 +1,83 @@
+const projectContainer = document.querySelector('#projectContainer');
+
+async function getProjects() {
+  try {
+    const result = await fetch('./projects/projects.json');
+    const projects = await result.json();
+    makeUrl(projects);
+    generateHTML(projects);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function makeUrl(projects) {
+  projects.forEach((project) => {
+    project.url = project.title.toLowerCase().split(' ').join('-');
+  });
+}
+
+function generateTagsForHTML(project, tagName, className) {
+  return project.tags.includes(tagName) ? className : 'hidden';
+}
+
+function generateHTML(projects) {
+  projects.forEach((project) => {
+    const html = `
+    <article class="project">
+      <div class="project-image">
+        <img
+          src="./projects/${project.url}/docs/design/desktop-preview.jpg"
+          alt="${project.title}"
+        />
+      </div>
+      <div class="project-info">
+        <div class="project-tags">
+          <span class="project-tag ${generateTagsForHTML(
+            project,
+            'html',
+            'project-tag--html'
+          )}"> HTML </span>
+          <span class="project-tag ${generateTagsForHTML(
+            project,
+            'css',
+            'project-tag--css'
+          )}"> CSS </span>
+          <span class="project-tag ${generateTagsForHTML(
+            project,
+            'js',
+            'project-tag--js'
+          )}"> JS </span>
+          <span class="project-tag ${generateTagsForHTML(
+            project,
+            'api',
+            'project-tag--api'
+          )}"> API </span>
+        </div>
+        <h2 class="project-title">${project.title}</h2>
+        <div class="project-buttons">
+          <a
+            href="https://github.com/SalahuddinAhammed/frontend-mentor-solutions/tree/main/projects/${
+              project.url
+            }"
+            target="_blank"
+            class="btn"
+            >Code</a
+          >
+          <a
+            href="https://frontend-mentor-solutions-salahuddin.netlify.app/projects/${
+              project.url
+            }"
+            target="_blank"
+            class="btn"
+            >Live</a
+          >
+        </div>
+      </div>
+    </article>
+  `;
+    projectContainer.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
+getProjects();
