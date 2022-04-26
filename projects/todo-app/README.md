5e41de998ab276fbf50196896df5ab445788c502
Salahuddin Ahammed <salahuddinahammed@yahoo.com>
2022-04-26 15:27:03 +0600

    feat: new project - todo app

---
+++ projects/todo-app/README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Todo app solution
+
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Frontend Mentor - Todo app solution](#frontend-mentor---todo-app-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+  - [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- [x] View the optimal layout for the app depending on their device's screen size
+- [x] See hover states for all interactive elements on the page
+- [x] Add new todos to the list
+- [x] Mark todos as complete
+- [x] Delete todos from the list
+- [x] Filter by all/active/complete todos
+- [x] Clear all completed todos
+- [x] Toggle light and dark mode
+- [ ] **Bonus**: Drag and drop to reorder items on the list
+
+### Screenshot
+
+![](./docs/screenshot.jpeg)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/SalahuddinAhammed/frontend-mentor-solutions/tree/main/projects/todo-app)
+- Live Site URL: [Netlify](https://frontend-mentor-solutions-salahuddin.netlify.app/projects/todo-app)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+
+## Author
+
+- Frontend Mentor - [@SalahuddinAhammed](https://www.frontendmentor.io/profile/salahuddinahammed)
+- GitHub - [@SalahuddinAhammed](https://www.github.com/SalahuddinAhammed)
---
+++ projects/todo-app/js/script.js
@@ -0,0 +1,155 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+const newTodo = document.querySelector(".todo__new-todo");
+const inputNewTodo = document.querySelector(".todo__new-todo-input");
+const todoWrapperEl = document.querySelector(".todo-wrapper");
+const newTodoIcon = document.querySelector(".todo__icon--new-todo");
+const btnClearCompleted = document.querySelector(
+  ".container--todo__clear-completed"
+);
+const btnsTodoSort = document.querySelectorAll(".container--todo__sort");
+const btnThemeSwitcher = document.querySelector(".header__theme-switcher");
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+localStorage.setItem("todos", JSON.stringify(todos));
+
+const updateLocalStorage = (task, options) => {
+  todos = JSON.parse(localStorage.getItem("todos"));
+  if (task === "deleteTodo" || task === "completeTodo") {
+    const targetedTodo = event.target.closest(".todo");
+    if (task === "completeTodo") {
+      todos.forEach((todo) => {
+        if (todo.id === targetedTodo.getAttribute("data-id")) {
+          todo.status === "uncompleted"
+            ? (todo.status = "completed")
+            : (todo.status = "uncompleted");
+        }
+      });
+    } else {
+      targetedTodo.style.display = "none";
+      todos = todos.filter(
+        (todo) => todo.id !== targetedTodo.getAttribute("data-id")
+      );
+    }
+  } else if (task === "createTodo") {
+    todos.push({
+      id: uuidv4(),
+      title: options.title,
+      status: event.target
+        .closest(".todo__new-todo")
+        .classList.contains("todo__new-todo--completed")
+        ? "completed"
+        : "uncompleted",
+    });
+  } else if (task === "clearCompleted") {
+    todos = todos.filter((todo) => todo.status !== "completed");
+  } else {
+    alert("Something wrong happened!");
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const generateHTML = () => {
+  todoWrapperEl.innerHTML = "";
+  todos = JSON.parse(localStorage.getItem("todos"));
+  todos.forEach((todo) => {
+    const html = `<div class="todo ${
+      todo.status === "completed" ? "todo--completed" : ""
+    }" data-id=${todo.id}>
+    <div class="todo__icon-wrapper">
+      <div class="todo__icon">
+        <img
+          src="./images/icon-check.svg"
+          alt="check icon"
+          class="todo__icon-image"
+        />
+      </div>
+    </div>
+    <p class="todo__title">${todo.title}</p>
+    <div class="todo__delete">
+      <img src="./images/icon-cross.svg" alt="Cross icon" />
+    </div>
+  </div>`;
+    todoWrapperEl.insertAdjacentHTML("afterbegin", html);
+  });
+  const btnsTodoDelete = document.querySelectorAll(".todo__delete");
+  const btnsTodoIcon = document.querySelectorAll(".todo__icon");
+
+  if (btnsTodoDelete) {
+    btnsTodoDelete.forEach((btnTodoDelte) => {
+      btnTodoDelte.addEventListener("click", (event) => {
+        updateLocalStorage("deleteTodo");
+        countUncompletedTodo();
+      });
+    });
+  }
+
+  if (btnsTodoIcon) {
+    btnsTodoIcon.forEach((btnTodoIcon) => {
+      btnTodoIcon.addEventListener("click", (event) => {
+        const targetedTodo = event.target.closest(".todo");
+        targetedTodo.classList.toggle("todo--completed");
+        updateLocalStorage("completeTodo");
+        countUncompletedTodo();
+      });
+    });
+  }
+};
+
+generateHTML();
+
+const addTodo = () => {
+  const textNewTodo = inputNewTodo.value;
+  if (!textNewTodo) return;
+  updateLocalStorage("createTodo", { title: textNewTodo });
+  generateHTML();
+};
+
+newTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addTodo();
+  inputNewTodo.value = "";
+  countUncompletedTodo();
+});
+
+btnClearCompleted.addEventListener("click", () => {
+  updateLocalStorage("clearCompleted");
+  generateHTML();
+  countUncompletedTodo();
+});
+
+const countUncompletedTodo = () => {
+  let uncompletdTodoNumber = 0;
+  const labelUncompletedTodo = document.querySelector(
+    ".contianer--todo__number"
+  );
+  JSON.parse(localStorage.getItem("todos")).forEach((todo) => {
+    if (todo.status === "uncompleted") {
+      uncompletdTodoNumber++;
+    }
+  });
+  labelUncompletedTodo.textContent = uncompletdTodoNumber;
+};
+
+countUncompletedTodo();
+
+btnsTodoSort.forEach((btnTodoSort) => {
+  btnTodoSort.addEventListener("click", (e) => {
+    btnsTodoSort.forEach((btnTodoSort) => {
+      btnTodoSort.classList.remove("container--todo__sort--active");
+    });
+    e.target.classList.add("container--todo__sort--active");
+    const sortOption = e.target.getAttribute("data-sort");
+    todoWrapperEl.setAttribute("data-sort", sortOption);
+  });
+});
+
+newTodoIcon.addEventListener("click", (event) => {
+  event.target
+    .closest(".todo__new-todo")
+    .classList.toggle("todo__new-todo--completed");
+});
+
+btnThemeSwitcher.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-mode");
+});
